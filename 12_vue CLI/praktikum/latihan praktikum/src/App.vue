33bd7929933595fd77ad9ada25b635f1366a4e0c
gisela ramadinawati <gigisgisela2111@gmail.com>
2022-03-18 20:16:27 +0700

    praktikum

---
+++ 12_vue CLI/praktikum/latihan praktikum/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <ol>
+      <li v-for="(messege, index) in messeges" :key="messege.id">
+        {{ messege }}
+        <button v-on:click="deleteList(index)">Hapus</button>
+      </li>
+      
+    </ol>
+    
+    <p v-if="messeges.length == 0">List kegiatan anda masih kosong</p>
+
+    <input type="text" v-on:keyup.enter="addList(messege)" v-model="messege">
+    <button v-on:click="addList(messege)">Tambahkan</button>
+
+    <p v-if="messeges.length >= 4">Hebat!</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      messege : "",
+      messeges : []
+    }
+  },
+  methods : {
+    addList(messege) {
+      if (messege == "") {
+        alert("Data yang dimasukan tidak boleh kosong")
+      } else {
+        this.messeges.push(this.messege)
+        this.messege = ""
+      }
+      
+    },
+    deleteList(index) {
+      this.messeges.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
